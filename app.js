7ba634a284faac74d2bbf6a435540ac632ec22df
lreboseno22 <liamrebo22@gmail.com>
2022-04-12 15:45:08 -0400

    Client requests

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+
 const taskRouter = require('./Routes/taskRouter')
 
 const app = express()
 const PORT = 9000
 
 app.use(express.json())
-
+app.use(cors())
 app.use(taskRouter)
 
 app.listen(PORT, () => {
