2756a28fa08140ac59f88ea123169d3db74dafe2
Uzma Khan <75581782+ukhan12@users.noreply.github.com>
2022-04-13 15:50:16 -0400

    Ubranch (#8)
    * add project table and routes
    
    * routes work
    
    Co-authored-by: Jah821 <91166820+Jah821@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const taskRouter = require('./Routes/taskRouter')
+const projectRouter = require('./Routes/projectsRouter')
 
 const app = express()
 const PORT = 9000
@@ -9,6 +10,7 @@ const PORT = 9000
 app.use(express.json())
 app.use(cors())
 app.use(taskRouter)
+app.use(projectRouter);
 
 app.listen(PORT, () => {
 console.log(`Listening on port ${PORT}`)
