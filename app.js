aebc0495caf0c07b51d80f4737a99e77d5dfc5b6
Jah821 <91166820+Jah821@users.noreply.github.com>
2022-04-13 17:34:26 -0400

    Created user routes,models and cotrollers,all routes work from postman

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const taskRouter = require('./Routes/taskRouter')
-const projectRouter = require('./Routes/projectsRouter')
+const taskRouter = require('./Routes/taskRouter');
+const projectRouter = require('./Routes/projectsRouter');
+const userRouter = require('./Routes/userRouter');
 
-const app = express()
+
+const app = express();
 const PORT = 9000
 
-app.use(express.json())
-app.use(cors())
-app.use(taskRouter)
-app.use(projectRouter);
+app.use(express.json());
+app.use(cors());
+app.use(taskRouter);;
+app.use(projectRouter);;
+app.use(userRouter);;
+
 
 app.listen(PORT, () => {
 console.log(`Listening on port ${PORT}`)
-})
+});
