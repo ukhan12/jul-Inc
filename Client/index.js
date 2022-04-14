63efc6e7081bd2cc75ade165877ed45ae5b8c07f
lreboseno22 <liamrebo22@gmail.com>
2022-04-13 21:32:07 -0400

    Homepage and Login

--- Client/index.js
+++ Client/index.js
@@ -1,17 +1,8 @@
 // Brower JS Code
 
 const url = "http://localhost:9000/tasks"
-
-document.querySelector('#create-task-form').addEventListener('submit', createTask)
-
-// function renderTasks(task){
-//     const listItem = document.createElement("li");
-//     listItem.id = `task:${task.id}`;
-//     const description = document.createElement('p');
-//     description.innerText = task.name;
-//     description.id = `description:${task.id}`;
-//     listItem.append('description')
-// }
+      
+document.getElementById('create-task-form').addEventListener('submit', createTask)
 
 function createTask(event){
   event.preventDefault()
@@ -26,3 +17,85 @@ function createTask(event){
     body: JSON.stringify({task: desc})
   })
 }
+
+function renderTasks(task){
+  const listItem = document.createElement('div')
+  listItem.setAttribute("id", "to-do")
+  listItem.setAttribute("class", "col, item,")
+  listItem.setAttribute("draggable", "true")
+
+  const description = document.createElement('div')
+  description.setAttribute("class", "p-3 border bg-light")
+  description.setAttribute("id", "content-box")
+  // description.setAttribute("draggable", "true")
+  
+  console.log(task)
+  description.innerText = task.Task
+  
+  listItem.append(description)
+
+  const toDoContainer = document.getElementById('to-do-container')
+  toDoContainer.append(listItem)
+
+  
+  // DRAGGABLE TASK ITEM FUNCTIONALITY 
+    // listItem.addEventListener('dragstart', dragStart);
+    // function dragStart(e) {
+    //   e.dataTransfer.setData('text/plain', e.target.id);
+    //   setTimeout(() => {
+    //       e.target.classList.add('hide');
+    //   }, 0);
+    // }
+    // /* drop targets */
+    // const boxes = document.querySelectorAll('.box-container');
+
+    // boxes.forEach(box => {
+    //   box.addEventListener('dragenter', dragEnter)
+    //   box.addEventListener('dragover', dragOver);
+    //   box.addEventListener('dragleave', dragLeave);
+    //   box.addEventListener('drop', drop);
+    // });
+
+    // function dragEnter(e) {
+    //   e.preventDefault();
+    //   e.target.classList.add('drag-over');
+    // }
+
+    // function dragOver(e) {
+    //   e.preventDefault();
+    //   e.target.classList.add('drag-over');
+    // }
+
+    // function dragLeave(e) {
+    //   e.target.classList.remove('drag-over');
+    // }
+
+    // function drop(e) {
+    //   e.target.classList.remove('drag-over');
+
+    //   // get the draggable element
+    //   const id = e.dataTransfer.getData('text/plain');
+    //   const draggable = document.getElementById(id);
+
+    //   // add it to the drop target
+    //   e.target.appendChild(draggable);
+
+    //   // display the draggable element
+    //   draggable.classList.remove('hide');
+    // }
+} 
+
+function iterateThroughData(tasks){
+  console.log(tasks)
+  tasks.forEach(renderTasks)
+}
+
+function initialFetchTasks(){
+  fetch(url)
+  .then(res => res.json())
+  .then(data => iterateThroughData(data.data))
+}
+initialFetchTasks()
+
+
+
